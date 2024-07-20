8c63dc837d336f018368997af4b14b93f078a0d1
lino tom <linotom04@gmail.com>
2024-07-20 15:01:32 +0530

    ViewAllMovie Created

---
+++ src/components/ViewAllMovies.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const ViewAllMovies = () => {
+  return (
+    <div>
+        <div className="container">
+            <div className="row">
+                <div className="col col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Movie Name</th>
+      <th scope="col">Actor</th>
+      <th scope="col">Actress</th>
+      <th scope="col">Director</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">Thor</th>
+      <td>Chris Hemsworth</td>
+      <td>Natalie Portman</td>
+      <td>charles</td>
+    </tr>
+  </tbody>
+</table>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ViewAllMovies
